f0c81cc8da9fc3a80e873462bfb15dfc779424e2
Peng Li <jylipeng@tju.edu.cn>
2021-11-01 23:56:29 +0800

    create spring project

---
+++ src/main/java/com/awesomespring/awesomespring/AwesomespringApplication.java
@@ -0,0 +1,13 @@
+package com.awesomespring.awesomespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomespringApplication.class, args);
+	}
+
+}

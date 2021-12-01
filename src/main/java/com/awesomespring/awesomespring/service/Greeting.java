e5d8e070fe0f00d00de8287ddfcf94561d24309c
Peng Li <jylipeng@tju.edu.cn>
2021-12-01 23:47:59 +0800

    add the restful demo

---
+++ src/main/java/com/awesomespring/awesomespring/service/Greeting.java
@@ -0,0 +1,17 @@
+package com.awesomespring.awesomespring.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Greeting {
+	private long id;
+	private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}

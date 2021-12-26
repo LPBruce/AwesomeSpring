a952cd45ef3e87cfb95638ef9fda63c72e952284
Peng Li <jylipeng@tju.edu.cn>
2021-12-26 23:40:16 +0800

    update

--- src/main/java/com/awesomespring/awesomespring/service/vo/Taco.java
+++ src/main/java/com/awesomespring/awesomespring/service/vo/Taco.java
@@ -1,13 +1,17 @@
 package com.awesomespring.awesomespring.service.vo;
 
 import java.util.List;
+
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class Taco {
     
     @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
     private List<String> ingredients;
 }
